650a6ede49dc37027599653ad6011d8cc09dfd5b
danielmeloalencar <danielm.alencar@hotmail.com>
2020-12-11 16:49:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
+
 const { proxyServer } = require('tcp-local-tunnel');
  
 proxyServer({
   proxyPort: 8888, // remote port to access exposed local machine
-  tunnelPort: 8885 // tunnel port
+  tunnelPort: 8010 // tunnel port
 });
