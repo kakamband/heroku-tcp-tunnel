5e2983cdcb48072cdeef75e384ec46513631b638
danielmeloalencar <danielm.alencar@hotmail.com>
2020-12-11 03:00:48 -0300

    uptade

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const { proxyServer } = require('tcp-local-tunnel')
+const express = require('express');
+
+
+
+const serverPort = 3000;
+
+const server = express();
+
+
+
+server.get('/', function(req, res, next) {
+  res.send('ok');
+});
+
+server.listen(serverPort);
+var servidor= require('http').createServer(servidor);
 
 proxyServer({
   proxyPort: 80, // remote port to access exposed local machine
