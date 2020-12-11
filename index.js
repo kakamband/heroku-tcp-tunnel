ed8e56f7cd69725681ecc7a1302f863a194417cd
danielmeloalencar <danielm.alencar@hotmail.com>
2020-12-11 02:17:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const { client } = require('tcp-local-tunnel');
-client(
-  {
-    host: '255.255.255.255', // remote server ip or domain
-    port: 8010 // tunnel port
-  },
-  {
-    host: 'localhost',
-    port: 3000
-  }
-);
+
+proxyServer({
+  proxyPort: 80, // remote port to access exposed local machine
+  tunnelPort: 8010 // tunnel port
+});
